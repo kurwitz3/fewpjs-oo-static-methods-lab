b2669dda0c9a474eca7c340e58b0f88471be7f78
kurwitz3 <kurwitz3@aol.com>
2021-02-09 21:27:37 -0500

    commit tree 6f0613e10b35d913008417123ac9d5fa74b09cbc

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+   let letter = string.charAt(0).toUpperCase() + string.slice(1)
+   return letter
+  }
+
+  static sanitize(string){
+  return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string){
+    const words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(" ").map((word, i)=> words.includes(word) && i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  }
 }
